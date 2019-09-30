5364dafcab2c8f6399b4d2ca81a8c37a959c76bd
kakizakimaki <xxxmaki112@gmail.com>
2019-09-30 17:06:01 +0900

    ログイン画面のView

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'home#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  resources :users
-
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   
+  resources :users
+
   get '/index' => 'home#index'
   post '/callback' => 'linebot#callback'
 end
