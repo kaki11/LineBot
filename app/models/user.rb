8be7004a678326e144321c8ad3ff304b2c0a4a23
kakizakimaki <xxxmaki112@gmail.com>
2019-09-30 16:07:01 +0900

    validationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, uniqueness: true, format: { with:/\A\S+@\S+\.\S+\z/ }
+  validates :password, confirmation: true, length: { in: 8..32 }, format: { with:/(?=.*?[a-zA-Z])(?=.*?\d)[\w]/ }
 
   has_secure_password
 end
