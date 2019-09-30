46a3336a31e8e9f9c1116e071dc53737a0373ec8
kakizakimaki <xxxmaki112@gmail.com>
2019-09-30 15:16:26 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
   get '/index' => 'home#index'
   post '/callback' => 'linebot#callback'
