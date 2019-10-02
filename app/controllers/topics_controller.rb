7bf5bdf44eab81c2b53119ebea3636fcc30f55b3
kakizakimaki <xxxmaki112@gmail.com>
2019-10-02 10:26:09 +0900

    削除機能追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,6 @@
 class TopicsController < ApplicationController
+  before_action :set_topic_id, only: [:edit, :update, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy]
 
   def index
     @topics = Topic.all
@@ -11,16 +13,27 @@ class TopicsController < ApplicationController
   def create
     @topic = current_user.topics.new(topic_params)
     if @topic.save
-      redirect_to topics_path, success: '登録しました'
+      redirect_to topics_path
     else
       flash.now[:danger] = "登録に失敗しました"
       render :new
     end
   end
 
+  def destroy
+    # @topic = topics.find_by(id: params[:id])
+    @topic.destroy
+      redirect_to topics_path
+  end
+
   private
+
   def topic_params
     params.require(:topic).permit(:send_message, :receive_message)
+  end
+
+  def set_topic_id
+    @topic = Topic.find(params[:id])
   end
 
 end
