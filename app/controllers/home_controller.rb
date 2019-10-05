8a2540fa5989b6a5e3a2c235fdf64dad5df40d2e
kakizakimaki <xxxmaki112@gmail.com>
2019-10-05 16:43:02 +0900

    ユーザー情報の取得

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   end
 
   def show
+  #アクセストークンの取得
     uri = URI.parse('https://api.line.me/oauth2/v2.1/token')
     res = Net::HTTP.post_form(uri, { 
       client_id: "1626757935",
@@ -12,6 +13,16 @@ class HomeController < ApplicationController
       redirect_uri: "http://localhost:3000/show"
     })
     puts res.body
+    
+    #ユーザー情報の取得
+      uri = URI.parse("https://api.line.me/v2/profile")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme === "https"
+      headers = { Authorization: "Bearer access_token" }
+      response = http.get(uri.path, headers)
+      puts response.code 
+      puts response.body 
+
   end
 
   def create
